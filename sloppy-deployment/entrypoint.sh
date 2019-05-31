d3c4df7d18a65b4ea6afeecc76b686424b44f85a
Markus "Shorty" Uckelmann <markus.uckelmann@koeln.de>
2019-05-31 19:48:50 +0200

    sloppy-deployment: make it DRY
    Move copying of sloppy.yml into a function.

--- sloppy-deployment/entrypoint.sh
+++ sloppy-deployment/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -eu
-set -x
 
 SLOPPY_OUTPUT_FILE="sloppy.yml"
 safe_yaml="${SLOPPY_SAVE_SLOPPY_YAML:-}"
+
+function cp_sloppy_yml () {
+  if [[ $safe_yaml ]] ; then
+     cp "$SLOPPY_OUTPUT_FILE" "$safe_yaml"
+  fi
+}
 
 gomplate -f "/work/${SLOPPY_TEMPLATE_FILE}" \
   -d environment="file:///environments/${ENVIRONMENT}/" \
@@ -11,12 +16,8 @@ gomplate -f "/work/${SLOPPY_TEMPLATE_FILE}" \
 
 if [[ -n "$DRY_RUN" ]]; then
   cat "${SLOPPY_OUTPUT_FILE}"
-  if [[ $safe_yaml ]] ; then
-     cp "$SLOPPY_OUTPUT_FILE" "$safe_yaml"
-  fi
+  cp_sloppy_yml
 else
   sloppy change -f $SLOPPY_OUTPUT_FILE
-  if [[ $safe_yaml ]] ; then
-     cp "$SLOPPY_OUTPUT_FILE" "$safe_yaml"
-  fi
+  cp_sloppy_yml
 fi
