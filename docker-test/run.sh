4677501b298013410d2662daa45558ab28c54b33
Markus "Shorty" Uckelmann <markus.uckelmann@koeln.de>
2019-06-07 17:38:27 +0200

    add-testing: several improvements
    * adds docker-test/install_docker-compose.sh
    * adds wordpress/tests/test.sh for prep and teardown to run the tests
    
    Makefile:
    * adds help hack
    * adds clean target
    
    README.md:
    * adds documentation for tests
    
    docker-compose.ops.yml:
    * adds test network
    
    docker-test/Dockerfile:
    * fix docker-compose installation
    
    docker-test/README.md:
    * add text
    
    docker-test/run.sh:
    * minor improvements
    * adds logic to look for test.sh first
    
    wordpress/docker-compose.yml:
    * adds external test network and fixed name for wp container
    
    wordpress/tests/wordpress.bats:
    * removes prep and teardown stuff

--- docker-test/run.sh
+++ docker-test/run.sh
@@ -3,25 +3,28 @@
 set -euo pipefail
 
 CHANGES=$(git diff --name-only "$GIT_DIFF" | \
-	sort -u | \
-	awk 'BEGIN {FS="/"} {print $1}' | \
-	uniq | \
-	xargs -I % find . -type d -name % -exec basename {} \; \
+    sort -u | \
+    awk 'BEGIN {FS="/"} {print $1}' | \
+    uniq | \
+    xargs -I % find . -type d -name % -exec basename {} \; \
 )
 IMAGES="${IMAGES:-$CHANGES}"
 
-if [[ -z $IMAGES ]]
-then
-	echo "Nothing to test."
-	exit 0
+if [[ -z $IMAGES ]] ; then
+    echo "Nothing to test."
+    exit 0
 fi
 
 while IFS= read -r IMAGE_NAME
 do
-    cd "$IMAGE_NAME"
+    ( cd "$IMAGE_NAME"
     if [[ -d ./tests ]] ; then
-        bats --tap ./tests
+        if [[ -f tests/test.sh ]] ; then
+            ./tests/test.sh
+        else
+            bats --tap ./tests
+        fi
     else
         echo "No tests found for $IMAGE_NAME"
-    fi
+    fi )
 done < <(echo "$IMAGES" | tr " " "\n")
--- wordpress/tests/wordpress.bats
+++ wordpress/tests/wordpress.bats
@@ -1,30 +1,11 @@
 #!/usr/bin/env bats
 
 
-@test "compose up" {
-    run docker-compose up -d --build --force-recreate
-    echo "$output"
-    [[ $status -eq 0 ]]
-}
-
-@test "blah" {
-    sleep 20
-    run curl localhost:8008
-    echo "$output"
-    [[ $output == blubb ]]
-}
-
-@test "wp GET /" {
+@test "wp GET / on 80" {
     # FIXME use something like wait-for-it
     #sleep 20
-    run curl -s -o /dev/null -w "%{http_code}" localhost:8008
+    run curl -s -o /dev/null -w "%{http_code}" "$MY_IP:80"
     echo "$output"
     [[ $status -eq 0 ]]
-    [[ $output == 200 ]]
-}
-
-@test "compose down" {
-    run docker-compose down -v --remove-orphans
-    echo "$output"
-    [[ $status -eq 0 ]]
+    [[ $output -eq 301 ]]
 }
