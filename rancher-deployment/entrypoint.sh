1a6d24984f80d6f2fc12034637b5e6422d055656
Markus "Shorty" Uckelmann <markus.uckelmann@koeln.de>
2019-07-02 11:13:05 +0200

    hotfix: fix fail when vars not set
    rancher-deployment/entrypoint.sh:
    * adds DEBUG switch
    * fixes fail when vars ( DRY_RUN & SETUP_VOLUMES ) are not set
    * fixes Shellcheck warnings
    
    rancher-deployment/tests/rancher.bats:
    * adds test for SETUP_VOLUMES not set

--- rancher-deployment/entrypoint.sh
+++ rancher-deployment/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -eu
 
+[[ "$RANCHER_DEPLOYMENT_TRACE" ]] && set -x
+
 DOCKER_COMPOSE_OUTPUT_FILE="./docker-compose.yml"
 RANCHER_COMPOSE_OUTPUT_FILE="./rancher-compose.yml"
 
+# Should we make a dry run
+DRY_RUN="${DRY_RUN:-}"
+# Should we setup any volumes
+SETUP_VOLUMES="${SETUP_VOLUMES:-}"
 # Name of the directory (should be a host volume) to where a copy of
 # $DOCKER_COMPOSE_OUTPUT_FILE and RANCHER_COMPOSE_OUTPUT_FILE is saved.
 RANCHER_SAVE_OUTPUT_DIR="${RANCHER_SAVE_OUTPUT_DIR:-}"
@@ -25,7 +31,8 @@ if [[ -n $RANCHER_SAVE_OUTPUT_DIR ]] ; then
 fi
 
 if [[ -n "$SETUP_VOLUMES" ]]; then
-  . ./setup-volumes.sh
+    # shellcheck disable=SC1091
+    . ./setup-volumes.sh
 fi
 
 if [[ -n "$DRY_RUN" ]]; then
@@ -34,12 +41,12 @@ if [[ -n "$DRY_RUN" ]]; then
   cat "${RANCHER_COMPOSE_OUTPUT_FILE}"
 else
   echo "Upgrading $ENVIRONMENT ..."
-  rancher up -f "${DOCKER_COMPOSE_OUTPUT_FILE}" --rancher-file "${RANCHER_COMPOSE_OUTPUT_FILE}" --upgrade --pull -d --force-upgrade --stack "$STACK" $SERVICE
+  rancher up -f "${DOCKER_COMPOSE_OUTPUT_FILE}" --rancher-file "${RANCHER_COMPOSE_OUTPUT_FILE}" --upgrade --pull -d --force-upgrade --stack "$STACK" "$SERVICE"
   rancher --wait-state upgraded wait
   rancher --wait-state healthy wait
 
   echo "Confirming upgrade for $ENVIRONMENT ..."
-  rancher up -f "${DOCKER_COMPOSE_OUTPUT_FILE}" --rancher-file "${RANCHER_COMPOSE_OUTPUT_FILE}" --confirm-upgrade -d --stack "$STACK" $SERVICE
+  rancher up -f "${DOCKER_COMPOSE_OUTPUT_FILE}" --rancher-file "${RANCHER_COMPOSE_OUTPUT_FILE}" --confirm-upgrade -d --stack "$STACK" "$SERVICE"
   rancher --wait-state active wait
   rancher --wait-state healthy wait
 fi
