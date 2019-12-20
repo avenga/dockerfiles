e53fb60c59b534ca375ffba792581fe84306b6c8
Krispin Schulz <krispin.schulz@sevenval.com>
2019-12-20 16:42:43 +0100

    experimental work in progress

--- rancher2-deployment/entrypoint.sh
+++ rancher2-deployment/entrypoint.sh
@@ -11,6 +11,15 @@ IFS=$'\n\t'
 ### define/initialize script wide variables
 
 PROGNAME="${0##*/}"
+OUTPUT_FILE="./deployment.yml"
+# Make a dry run:
+DRY_RUN="${DRY_RUN:-}"
+RANCHER_PROJECT="${RANCHER_PROJECT:-$PROJECT}"
+# Use $PROJECT as prefix to have unique namespaces inside Rancher2:
+RANCHER_NAMESPACE="$(echo "${RANCHER_PROJECT}-${ENVIRONMENT}" | tr '[:upper:]' '[:lower:]')"
+# Name of the directory (should be a host volume) to where a copy of
+# $OUTPUT_FILE is saved.
+RANCHER_SAVE_OUTPUT_DIR="${RANCHER_SAVE_OUTPUT_DIR:-}"
 
 ################################################################################
 ### functions
@@ -32,17 +41,35 @@ function _bail () {
 
 ### main script
 
+echo "Deploy ${RANCHER_PROJECT} to ${RANCHER_NAMESPACE}"
 
-rancher login "$RANCHER_SERVER_URL" --token "$RANCHER_BEARER_TOKEN"
-rancher context switch "$RANCHER_PROJECT"
+gomplate -f "/work/${DEPLOYMENT_TEMPLATE_FILE}" \
+  -d environment="file:///environments/${ENVIRONMENT}/" \
+  -d globals="file:///environments/" \
+  -o "${OUTPUT_FILE}"
 
-RANCHER_NAMESPACE="$(echo "$RANCHER_PROJECT" | tr '[:upper:]' '[:lower:]')"
-if rancher namespace ls --format '{{.Namespace.Name}}' | grep -q "^${RANCHER_NAMESPACE}$" ; then
-    echo "Namespace $RANCHER_NAMESPACE already exists."
-else
-    rancher namespace create "$RANCHER_NAMESPACE"
+if [[ -n $RANCHER_SAVE_OUTPUT_DIR ]] ; then
+  cp "$OUTPUT_FILE" "$RANCHER_SAVE_OUTPUT_DIR"
 fi
 
-rancher kubectl apply --namespace "$RANCHER_NAMESPACE" -f ./deployment.yml
+if [[ -n "$DRY_RUN" ]]; then
+    cat "${OUTPUT_FILE}"
+else
+    # TODO: make context configurable:
+    rancher login "$RANCHER_SERVER_URL" --token "$RANCHER_BEARER_TOKEN" --context "c-p8799:p-7xmzd"
+
+    if rancher projects ls --format '{{.Project.Name}}' | grep -q "^${RANCHER_PROJECT}$" ; then
+        echo "Project $RANCHER_PROJECT already exists."
+    else
+        rancher projects create "$RANCHER_PROJECT"
+    fi
+    rancher context switch "$RANCHER_PROJECT"
+    if rancher namespace ls --format '{{.Namespace.Name}}' | grep -q "^${RANCHER_NAMESPACE}$" ; then
+        echo "Namespace $RANCHER_NAMESPACE already exists."
+    else
+        rancher namespace create "$RANCHER_NAMESPACE"
+    fi
+    rancher kubectl apply --namespace "${RANCHER_NAMESPACE}" -f "${OUTPUT_FILE}"
+fi
 
 # vim: ts=4 sw=4 expandtab ft=sh
