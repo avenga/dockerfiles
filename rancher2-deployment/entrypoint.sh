5c70c197b4aba9b9f8e793f2adde2f7b90ddce10
Markus "Shorty" Uckelmann <markus.uckelmann@koeln.de>
2020-01-24 16:32:27 +0100

    rancher2-deployment/adds_kubectl_diff
    README.md:
    * improves README
    
    entrypoint.sh:
    * adds more comments
    * adds "kubectl diff"

--- rancher2-deployment/entrypoint.sh
+++ rancher2-deployment/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# optionally set trace mode
+# When set trace mode is activated. See `help set` in Bash.
 [[ "$RANCHER2_DEPLOYMENT_TRACE" ]] && set -x
 
 # Bash Strict Mode, s. http://redsymbol.net/articles/unofficial-bash-strict-mode/
@@ -12,7 +12,7 @@ IFS=$'\n\t'
 
 PROGNAME="${0##*/}"
 OUTPUT_FILE="./deployment.yml"
-# Make a dry run:
+# Make a dry run
 DRY_RUN="${DRY_RUN:-}"
 RANCHER_PROJECT="${RANCHER_PROJECT:-$PROJECT}"
 # Use $PROJECT as prefix to have unique namespaces inside Rancher2:
@@ -20,6 +20,9 @@ RANCHER_NAMESPACE="$(echo "${RANCHER_PROJECT}-${ENVIRONMENT}" | tr '[:upper:]' '
 # Name of the directory (should be a host volume) to where a copy of
 # $OUTPUT_FILE is saved.
 RANCHER_SAVE_OUTPUT_DIR="${RANCHER_SAVE_OUTPUT_DIR:-}"
+# When set it turns off printing the difference between new and current
+# configuration to STDOUT.
+RANCHER_NO_DIFF="${RANCHER_NO_DIFF:-}"
 
 ################################################################################
 ### functions
@@ -42,7 +45,7 @@ function _bail () {
 ### main script
 
 echo "Deploy ${RANCHER_PROJECT} to ${RANCHER_NAMESPACE}"
-mkdir -p /environments/${ENVIRONMENT}
+mkdir -p "/environments/${ENVIRONMENT}"
 
 if [[ -z $DRY_RUN ]] ; then
     rancher login "$RANCHER_SERVER_URL" --token "$RANCHER_BEARER_TOKEN" --context "$RANCHER_CONTEXT"
@@ -73,8 +76,19 @@ if [[ -n $RANCHER_SAVE_OUTPUT_DIR ]] ; then
   cp "$OUTPUT_FILE" "$RANCHER_SAVE_OUTPUT_DIR"
 fi
 
+# Show diff between current and new configuration. Fails if the configuration
+# contains syntax or logical errors.
+if [[ -z "$RANCHER_NO_DIFF" ]] ; then
+    # Turn off exiting when a command fails, since if there is a diff found
+    # kubectl diff will exit non-zero.
+    set +e
+    rancher kubectl diff --namespace "${RANCHER_NAMESPACE}" -f "${OUTPUT_FILE}"
+    set -e
+fi
+
 if [[ -n "$DRY_RUN" ]]; then
-    # No kubectl apply executed; instead send generated k8s yml file to stdout:
+    # No kubectl apply is executed. Instead send generated k8s yml file to
+    # STDOUT
     cat "${OUTPUT_FILE}"
 else
     rancher kubectl apply --namespace "${RANCHER_NAMESPACE}" -f "${OUTPUT_FILE}"
