0d54e691b9156ba69a6656d1c4578bf46fd674df
Frederik Berg <frederik.berg@avenga.com>
2020-10-29 13:41:48 +0100

    push all tagged images instead of onyl latest

--- docker-push/run.sh
+++ docker-push/run.sh
@@ -13,26 +13,36 @@ if [[ -z $IMAGES ]] ; then
     )
 fi
 
-if [[ -z $IMAGES ]]
-then
+if [[ -z $IMAGES ]]; then
     echo "Nothing to push."
     exit 0
 fi
 
 if [[ -n "$DRY_RUN" ]]; then
-    echo "$IMAGES"
+    # for all images
+    for image in $IMAGES; do
+        # for all tags that are equal to latest
+        for tagged_image in $(docker image inspect --format $'{{range .RepoTags}}{{.}} {{end}}' "$IMAGE_PREFIX$image:latest"); do
+            # print it
+            echo "$tagged_image"
+        done
+    done
     exit 0
-else
-    # only push images if specific branch
-    if [[ "$CURRENT_BRANCH" == "$ONLY_BRANCH" ]]
-    then
-        echo "$CI_REGISTRY_PASSWORD" | docker login -u "$CI_REGISTRY_USER" "$CI_REGISTRY" --password-stdin
-        while IFS= read -r IMAGE_NAME
-        do
-            # push all tags:
-            docker push "$IMAGE_PREFIX$IMAGE_NAME"
-        done < <(echo "$IMAGES" | tr " " "\n")
-    else
-        echo "Skipping since not on ONLY_BRANCH $ONLY_BRANCH"
-    fi
 fi
+
+# only push images if specific branch
+if [[ "$CURRENT_BRANCH" != "$ONLY_BRANCH" ]]; then
+    echo "Skipping since not on ONLY_BRANCH $ONLY_BRANCH"
+    exit 0
+fi
+
+echo "$CI_REGISTRY_PASSWORD" | docker login -u "$CI_REGISTRY_USER" "$CI_REGISTRY" --password-stdin
+
+# for all images
+for image in $IMAGES; do
+    # for all tags that are equal to latest
+    for tagged_image in $(docker image inspect --format $'{{range .RepoTags}}{{.}} {{end}}' "$IMAGE_PREFIX$image"); do
+        # push it
+        docker push "$tagged_image"
+    done
+done
--- docker-push/tests/push.bats
+++ docker-push/tests/push.bats
@@ -1,7 +1,7 @@
 #!/usr/bin/env bats
 
 @test "dry run" {
-    run docker run --rm -e IMAGES=gitlab-job -e DRY_RUN=1  "avenga/docker-push:$VERSION"
+    run docker run --rm -e IMAGES=gitlab-job -e DRY_RUN=1 "avenga/docker-push:$VERSION"
     echo "$output"
     [[ $status -eq 0 ]]
     [[ $output == "gitlab-job" ]]
